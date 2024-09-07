c0b6c1e8d415476bf426477d7084fca6ecf5df54
sethukumaresan <sethu.kumareson@gmail.com>
2024-09-07 16:30:28 +0530

    Added Log files in order service

--- OrderService/orderservice/src/main/java/com/order/orderservice/OrderserviceApplication.java
+++ OrderService/orderservice/src/main/java/com/order/orderservice/OrderserviceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("microservice implementation");
 		SpringApplication.run(OrderserviceApplication.class, args);
 	}
 
